49cdca3dba8895ec2da76504cedba05fa2f7ed79
MinoUni <maks.reva.ua@gmail.com>
2022-07-18 13:55:48 +0300

    ProcessingQuestion: add base test implementation

--- src/main/java/com/eleks/academy/whoami/core/state/ProcessingQuestion.java
+++ src/main/java/com/eleks/academy/whoami/core/state/ProcessingQuestion.java
@@ -3,77 +3,121 @@ package com.eleks.academy.whoami.core.state;
 import com.eleks.academy.whoami.core.SynchronousPlayer;
 import com.eleks.academy.whoami.core.exception.GameException;
 import com.eleks.academy.whoami.core.exception.PlayerNotFoundException;
+import com.eleks.academy.whoami.model.response.PlayerState;
 import com.eleks.academy.whoami.model.response.PlayerWithState;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import com.eleks.academy.whoami.model.response.TurnDetails;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-// TODO: Implement makeTurn(...) and next() methods, pass a turn to next player
 public final class ProcessingQuestion implements GameState {
 
-	private final String currentPlayer;
-	
-	private final Map<String, PlayerWithState> players;
-	
-	public ProcessingQuestion(Map<String, PlayerWithState> players) {
-		this.players = players;
-		this.currentPlayer = players.keySet()
-				.stream()
-				.findAny()
-				.orElse(null);
-	}
+    private static final int DURATION = 1;
 
-	@Override
-	public GameState next() {
-		throw new GameException("Not implemented");
-	}
+    private static final TimeUnit UNIT = TimeUnit.MINUTES;
+    private final String currentPlayer;
 
-	@Override
-	public Optional<SynchronousPlayer> findPlayer(String player) {
-		var result = Optional.ofNullable(this.players.get(player));
-		if (result.isEmpty()) {
-			throw new PlayerNotFoundException("PROCESSING-QUESTION: [" + player + "] not found.");
-		}
-		return Optional.ofNullable(result.get().getPlayer());
-	}
+    private final Map<String, PlayerWithState> players;
 
-	public String getCurrentTurn() {
-		return this.currentPlayer;
-	}
+    private final Queue<PlayerWithState> playersQueue = new LinkedBlockingQueue<>();
 
-	@Override
-	public GameState getCurrentState() {
-		return this;
-	}
+    public ProcessingQuestion(Map<String, PlayerWithState> players) {
+        this.players = players;
+//        this.currentPlayer = players.keySet()
+//                .stream()
+//                .findAny()
+//                .orElse(null);
+        this.currentPlayer = players.get(3).getPlayer().getId();
+        this.players.forEach((k, v) -> {
+            if (k.equals(currentPlayer)) {
+                v.setState(PlayerState.ASKING);
+            } else v.setState(PlayerState.ANSWERING);
+        });
+        this.playersQueue.addAll(this.players.values());
+    }
 
-	@Override
-	public String getStatus() {
-		return this.getClass().getName();
-	}
+    @Override
+    public GameState next() {
+        throw new GameException("Not implemented");
+    }
+    //TODO: Implement cycle of Q-A
 
-	@Override
-	public boolean isReadyToNextState() {
-		return false;
-	}
+    public void gameCycle(String player, String message) {
 
-	@Override
-	public Optional<SynchronousPlayer> leave(String player) {
-		if (findPlayer(player).isPresent()) {
-			return Optional.of(this.players.remove(player).getPlayer());
-		} else throw new PlayerNotFoundException("[" + player + "] not found.");
-	}
+        if (player.equals(currentPlayer)) {
+            askQuestion(player, message);
+        } else {
+            answerQuestion(player, message);
+        }
 
-	@Override
-	public Stream<PlayerWithState> getPlayersList() {
-		return this.players.values().stream();
-	}
+    }
 
-	@Override
-	public String getPlayersInGame() {
-		return Integer.toString(this.players.size());
-	}
+    public void askQuestion(String player, String message) {
+
+    }
+
+    public void submitGuess(String player, String guess) {
+
+    }
+
+    public void answerQuestion(String player, String answer) {
+
+    }
+
+    public TurnDetails getTurnInfo() {
+        return new TurnDetails(this.players.get(currentPlayer).getPlayer(),
+                new ArrayList<>(this.players.values()));
+    }
+
+    @Override
+    public Optional<SynchronousPlayer> findPlayer(String player) {
+        var result = Optional.ofNullable(this.players.get(player));
+        if (result.isEmpty()) {
+            throw new PlayerNotFoundException("PROCESSING-QUESTION: [" + player + "] not found.");
+        }
+        return Optional.ofNullable(result.get().getPlayer());
+    }
+
+    public String getCurrentTurn() {
+        return this.currentPlayer;
+    }
+
+    @Override
+    public GameState getCurrentState() {
+        return this;
+    }
+
+    @Override
+    public String getStatus() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public boolean isReadyToNextState() {
+        return false;
+    }
+
+    @Override
+    public Optional<SynchronousPlayer> leave(String player) {
+        if (findPlayer(player).isPresent()) {
+            return Optional.of(this.players.remove(player).getPlayer());
+        } else throw new PlayerNotFoundException("[" + player + "] not found.");
+    }
+
+    @Override
+    public Stream<PlayerWithState> getPlayersList() {
+        return this.players.values().stream();
+    }
+
+    @Override
+    public String getPlayersInGame() {
+        return Integer.toString(this.players.size());
+    }
 
 }
