242d31975b00158a900ccbecb7c4a9c506ca2815
MinoUni <maks.reva.ua@gmail.com>
2022-07-24 17:03:05 +0300

    GameService: remove unused methods

--- src/main/java/com/eleks/academy/whoami/service/GameService.java
+++ src/main/java/com/eleks/academy/whoami/service/GameService.java
@@ -1,27 +1,34 @@
 package com.eleks.academy.whoami.service;
 
+import com.eleks.academy.whoami.core.SynchronousPlayer;
+import com.eleks.academy.whoami.model.request.CharacterSuggestion;
+import com.eleks.academy.whoami.model.request.GuessAnswer;
+import com.eleks.academy.whoami.model.request.NewGameRequest;
+import com.eleks.academy.whoami.model.request.QuestionAnswer;
+import com.eleks.academy.whoami.model.response.*;
+
 import java.util.List;
 import java.util.Optional;
 
-import com.eleks.academy.whoami.core.SynchronousPlayer;
-import com.eleks.academy.whoami.model.request.CharacterSuggestion;
-import com.eleks.academy.whoami.model.request.NewGameRequest;
-import com.eleks.academy.whoami.model.response.AllFields;
-import com.eleks.academy.whoami.model.response.GameDetails;
-import com.eleks.academy.whoami.model.response.GameLight;
-import com.eleks.academy.whoami.model.response.LeaveModel;
-import com.eleks.academy.whoami.model.response.QuickGame;
-import com.eleks.academy.whoami.model.response.TurnDetails;
-
 public interface GameService {
 
+	Integer getAllPlayersCount();
+
 	List<GameLight> findAvailableGames(String player);
+
+	Optional<GameDetails> findByIdAndPlayer(String id, String player);
+
+	List<AllFields> findAllGamesInfo(String player);
+
+	Optional<TurnDetails> findTurnInfo(String id, String player);
+
+	Optional<GameHistory> findGameHistory(String id);
 
 	Optional<GameDetails> createGame(String player, NewGameRequest gameRequest);
 
 	SynchronousPlayer enrollToGame(String id, String player);
 
-	Optional<GameDetails> findByIdAndPlayer(String id, String player);
+	Optional<LeaveModel> leaveGame(String id, String player);
 
 	void suggestCharacter(String id, String player, CharacterSuggestion suggestion);
 
@@ -29,17 +36,9 @@ public interface GameService {
 
 	void askQuestion(String gameId, String player, String message);
 
-	Optional<TurnDetails> findTurnInfo(String id, String player);
+	void answerQuestion(String id, String player, QuestionAnswer answer);
 
 	void submitGuess(String id, String player, String guess);
 
-	void answerQuestion(String id, String player, String answer);
-
-	Optional<QuickGame> findQuickGame(String player);
-
-	Optional<LeaveModel> leaveGame(String id, String player);
-
-	List<AllFields> findAllGamesInfo(String player);
-
-	Integer getAllPlayersCount();
+    void answerGuess(String id, String player, GuessAnswer answer);
 }
--- src/main/java/com/eleks/academy/whoami/service/impl/GameServiceImpl.java
+++ src/main/java/com/eleks/academy/whoami/service/impl/GameServiceImpl.java
@@ -2,12 +2,16 @@ package com.eleks.academy.whoami.service.impl;
 
 import com.eleks.academy.whoami.core.SynchronousGame;
 import com.eleks.academy.whoami.core.SynchronousPlayer;
+import com.eleks.academy.whoami.core.exception.GameException;
 import com.eleks.academy.whoami.core.exception.GameNotFoundException;
 import com.eleks.academy.whoami.core.exception.PlayerAlreadyInGameException;
 import com.eleks.academy.whoami.core.exception.PlayerNotFoundException;
+import com.eleks.academy.whoami.model.request.GuessAnswer;
+import com.eleks.academy.whoami.model.request.QuestionAnswer;
+import com.eleks.academy.whoami.model.response.GameHistory;
 import com.eleks.academy.whoami.core.impl.PersistentGame;
-import com.eleks.academy.whoami.core.state.ProcessingQuestion;
-import com.eleks.academy.whoami.core.state.SuggestingCharacters;
+import com.eleks.academy.whoami.core.state.impl.ProcessingQuestion;
+import com.eleks.academy.whoami.core.state.impl.SuggestingCharacters;
 import com.eleks.academy.whoami.model.request.CharacterSuggestion;
 import com.eleks.academy.whoami.model.request.NewGameRequest;
 import com.eleks.academy.whoami.model.response.*;
@@ -27,10 +31,43 @@ public class GameServiceImpl implements GameService {
 	private final GameRepository gameRepository;
 
 	@Override
+	public Integer getAllPlayersCount() {
+		return Math.toIntExact(this.gameRepository.getAllPlayers().count());
+	}
+
+	@Override
 	public List<GameLight> findAvailableGames(String player) {
 		return this.gameRepository.findAllAvailable(player)
 				.map(GameLight::of)
 				.toList();
+	}
+
+	@Override
+	public Optional<GameDetails> findByIdAndPlayer(String id, String player) {
+		return this.gameRepository.findById(id)
+				.filter(game -> game.findPlayer(player).isPresent())
+				.map(GameDetails::of);
+	}
+
+	@Override
+	public Optional<TurnDetails> findTurnInfo(String id, String player) {
+		var game = this.gameRepository.findById(id);
+		if (game.isPresent() && game.get().getState() instanceof ProcessingQuestion) {
+			return Optional.ofNullable(game.get().findTurnInfo(player));
+		} else throw new GameNotFoundException("PROCESSING-QUESTION: Game with id[" + id + "] not found.");
+	}
+
+	@Override
+	public Optional<GameHistory> findGameHistory(String id) {
+		var game = this.gameRepository.findById(id);
+		if (game.isPresent()) {
+			return Optional.ofNullable(game.get().getGameHistory());
+		} else throw new GameNotFoundException("findGameHistory: Game with id[" + id + "] not found.");
+	}
+
+	@Override
+	public List<AllFields> findAllGamesInfo(String player) {
+		return this.gameRepository.findAllGames(player).map(AllFields::of).toList();
 	}
 
 	@Override
@@ -54,19 +91,48 @@ public class GameServiceImpl implements GameService {
 
 	@Override
 	public SynchronousPlayer enrollToGame(String id, String player) {
-		
 		if (this.gameRepository.findPlayerByHeader(player).isPresent()) {
 			throw new PlayerAlreadyInGameException("ENROLL-TO-GAME: [" + player + "] already in other game.");
 		} else this.gameRepository.savePlayer(player);
-		
+
 		return this.gameRepository.findById(id).get().enrollToGame(player);
 	}
 
 	@Override
-	public Optional<GameDetails> findByIdAndPlayer(String id, String player) {
-		return this.gameRepository.findById(id)
-				.filter(game -> game.findPlayer(player).isPresent())
-				.map(GameDetails::of);
+	public Optional<LeaveModel> leaveGame(String id, String player) {
+
+		if (this.gameRepository.findPlayerByHeader(player).isPresent()) {
+
+			var game = this.gameRepository.findById(id);
+
+			if (game.isPresent()) {
+
+				this.gameRepository.deletePlayerByHeader(player);
+				var plToLeave = game.get().leaveGame(player).get();
+
+				if (String.valueOf(game.get().getPlayersList().size()).equals("0")) {
+					this.gameRepository.disbandGame(id);
+				}
+
+				return Optional.of(LeaveModel.of(plToLeave, id));
+
+			} else throw new GameNotFoundException("Game with id[" + id + "] not found.");
+
+		} else throw new PlayerNotFoundException("[" + player + "] in game with id[" + id + "] not found.");
+
+	}
+
+	@Override
+	public Optional<GameDetails> startGame(String id, String player) {
+		throw new GameException("Not implemented yet.");
+		/*
+		*	return this.gameRepository.findById(id)
+		* 			.filter(g -> g.getState().isReadyToNextState() &&
+		* 					g.getState() instanceof SuggestingCharacters
+		* 			)
+		* 			.map(SynchronousGame::start)
+		* 			.map(GameDetails::of);
+		* */
 	}
 
 	@Override
@@ -81,14 +147,6 @@ public class GameServiceImpl implements GameService {
 	}
 
 	@Override
-	public Optional<GameDetails> startGame(String id, String player) {
-		return this.gameRepository.findById(id)
-							.filter(g -> g.getState().isReadyToNextState() && g.getState() instanceof SuggestingCharacters)
-							.map(SynchronousGame::start)
-							.map(GameDetails::of);
-	}
-
-	@Override
 	public void askQuestion(String id, String player, String message) {
 		this.gameRepository.findById(id)
 				.filter(game -> game.getState() instanceof ProcessingQuestion)
@@ -100,11 +158,14 @@ public class GameServiceImpl implements GameService {
 	}
 
 	@Override
-	public Optional<TurnDetails> findTurnInfo(String id, String player) {
-		var game = this.gameRepository.findById(id);
-		if (game.isPresent()) {
-			return Optional.ofNullable(game.get().findTurnInfo(player));
-		} else throw new GameNotFoundException("PROCESSING-QUESTION: Game with id[" + id + "] not found.");
+	public void answerQuestion(String id, String player, QuestionAnswer answer) {
+		this.gameRepository.findById(id)
+				.filter(game -> game.getState() instanceof ProcessingQuestion)
+				.ifPresentOrElse(game -> game.answerQuestion(player, answer),
+						() -> {
+							throw new GameNotFoundException("PROCESSING-QUESTION: Game with id[" + id + "] not found.");
+						}
+				);
 	}
 
 	@Override
@@ -119,70 +180,14 @@ public class GameServiceImpl implements GameService {
 	}
 
 	@Override
-	public void answerQuestion(String id, String player, String answer) {
+	public void answerGuess(String id, String player, GuessAnswer answer) {
 		this.gameRepository.findById(id)
 				.filter(game -> game.getState() instanceof ProcessingQuestion)
-				.ifPresentOrElse(game -> game.answerQuestion(player, answer),
+				.ifPresentOrElse(game -> game.answerGuess(player, answer),
 						() -> {
 							throw new GameNotFoundException("PROCESSING-QUESTION: Game with id[" + id + "] not found.");
 						}
 				);
-	}
-
-	@Override
-	public Optional<QuickGame> findQuickGame(String player) {
-		
-		if (!this.gameRepository.findPlayerByHeader(player).isPresent()) {
-			
-			Map<String, SynchronousGame> games = gameRepository.findAvailableQuickGames();
-			
-			if (games.isEmpty()) {
-				
-				final SynchronousGame game = gameRepository.save(new PersistentGame(4));
-				enrollToGame(game.getId(), player);
-				
-				return gameRepository.findById(game.getId()).map(QuickGame::of);
-			}
-			
-			var FirstGame = games.keySet().stream().findFirst().get();
-			enrollToGame(games.get(FirstGame).getId(), player);
-			
-			return gameRepository.findById(games.get(FirstGame).getId()).map(QuickGame::of);
-		} else throw new PlayerAlreadyInGameException("QUICK-GAME: [" + player + "] already in other game.");
-	}
-
-	@Override
-	public Optional<LeaveModel> leaveGame(String id, String player) {
-		
-		if (this.gameRepository.findPlayerByHeader(player).isPresent()) {
-
-			var game = this.gameRepository.findById(id);
-			
-			if (game.isPresent()) {
-				
-				this.gameRepository.deletePlayerByHeader(player);
-				var plToLeave = game.get().leaveGame(player).get();
-
-				if (game.get().getPlayersInGame().equals("0")) {
-					this.gameRepository.disbandGame(id);
-				}
-
-				return Optional.of(LeaveModel.of(plToLeave, id));
-				
-			} else throw new GameNotFoundException("Game with id[" + id + "] not found.");
-			
-		} else throw new PlayerNotFoundException("[" + player + "] in game with id[" + id + "] not found.");
-
-	}
-
-	@Override
-	public List<AllFields> findAllGamesInfo(String player) {
-		return this.gameRepository.findAllGames(player).map(AllFields::of).toList();
-	}
-
-	@Override
-	public Integer getAllPlayersCount() {
-		return Math.toIntExact(this.gameRepository.getAllPlayers().count());
 	}
 
 }
